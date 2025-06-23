7d9f78a091f09df456462f50150895d100d3cfef
Munaf <munafmd9849@gmail.com>
2025-06-24 03:54:01 +0530

    Time: 0 ms (100%), Space: 12.3 MB (8.65%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        int n=nums.size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[i]<nums[j]&& nums[j]<nums[k] && nums[j]-nums[i]==diff && nums[k]-nums[j]==diff)
-                    {
-                        count++;
-                    }
-                }
-            }
+        
+        int cnt = 0;
+        
+        unordered_map<int,bool> mp;
+        
+        // Mark every elem presence in map.
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]] = true;
+        
+        
+        // For every element say 'elm' check if there exist both numbers, (elm + diff) and (elm - diff) inside map. If yes then increment cnt
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp[nums[i]-diff] && mp[nums[i]+diff])
+                cnt++;
         }
-        return count;
+        
+		
+		// Happy return :)
+        return cnt;
     }
 };
