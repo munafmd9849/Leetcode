fab8ec49c86b7e627388cff87a9691f9ef22eb68
Munaf <munafmd9849@gmail.com>
2025-09-23 22:56:54 +0530

    Time: 29 ms (16.77%), Space: 42.9 MB (5.3%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int> lm(n,-1);
+        stack<int> tem;
+        for(int i=0;i<n;i++){
+            while(tem.size()>0 && heights[tem.top()]>=heights[i]){
+                tem.pop();
+            }
+            if(tem.size()>0){
+                lm[i]=tem.top();
+            }
+            tem.push(i);
+        }
+
+        vector<int> rm(n,n);
+        stack<int> tem2;
+        for(int i=n-1;i>=0;i--){
+            while(tem2.size()>0 && heights[tem2.top()]>=heights[i]){
+                tem2.pop();
+            }
+            if(tem2.size()>0){
+                rm[i]=tem2.top();
+            }
+            tem2.push(i);
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int s=min(heights[i],(rm[i]-lm[i]-1));
+            ans=max(ans,s*s);
+        }
+        return ans;
+
+
+    }
+
+
+    int maximalSquare(vector<vector<char>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> matrix (n,vector<int> (m));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]=='1'){
+                    matrix[i][j]=1;
+                }
+                else{
+                     matrix[i][j]=0;
+                }
+            }
+        }
+
+        for(int j=0;j<m;j++){
+            int c=0;
+            for(int i=0;i<n;i++){
+                if(matrix[i][j]==1){
+                    c++;
+                }
+                else{
+                    c=0;
+                }
+                matrix[i][j]=c;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,largestRectangleArea(matrix[i]));
+        }
+        return ans;
+    }
+};
