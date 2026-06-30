f43d96bff57a4a6e7ce6aad80bd72c47402635bd
Munaf <191462807+munafmd9849@users.noreply.github.com>
2026-06-30 19:45:43 +0530

    Time: 0 ms (100%), Space: 13.9 MB (83.91%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -43,45 +43,72 @@
 
 
 
+// class Solution {
+// public:
+//     void fun(int i, int n,int sum,int t, vector<pair<int,int>>& can, vector<vector<int>> &ans, vector<int> &set){
+//         if(sum==t){
+//             ans.push_back(set);
+//             return;
+//         }
+//         if(sum>t || i==n){
+//             return;
+//         }
+//         fun(i+1,n,sum,t,can,ans,set);
+//         int x=can[i].first;
+//         int feq=can[i].second;
+//         for(int k=0;k<feq;k++){
+//             set.push_back(x);
+//             sum+=x;
+//             fun(i+1,n,sum,t,can,ans,set);
+//         }
+//         for(int k=0;k<feq;k++){
+//             set.pop_back();
+//             // sum-=x;
+//         }
+
+//     }
+
+//     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+//         vector<vector<int>> ans(0,vector<int>(0));
+//         vector<int> set(0);
+//         int n=candidates.size();
+//         unordered_map<int,int> um;
+//         for(int i=0;i<n;i++){
+//             um[candidates[i]]++;
+//         }
+//         vector<pair<int,int>> feq;
+//         for (auto& [key, value] : um){
+//             feq.push_back({key,value});
+//         }
+//         fun(0,feq.size(),0,target,feq,ans,set);
+//         return ans;
+
+//     }
+// };
+
+
 class Solution {
 public:
-    void fun(int i, int n,int sum,int t, vector<pair<int,int>>& can, vector<vector<int>> &ans, vector<int> &set){
-        if(sum==t){
-            ans.push_back(set);
+    void backtrack(int start, vector<int>& candidates, int target,
+                   vector<int>& curr, vector<vector<int>>& ans) {
+        if (target == 0) {
+            ans.push_back(curr);
             return;
         }
-        if(sum>t || i==n){
-            return;
+        for (int i = start; i < candidates.size(); i++) {
+            if (candidates[i] > target) break;                    
+            if (i > start && candidates[i] == candidates[i-1]) continue;  
+            curr.push_back(candidates[i]);
+            backtrack(i + 1, candidates, target - candidates[i], curr, ans);
+            curr.pop_back();                                      
         }
-        fun(i+1,n,sum,t,can,ans,set);
-        int x=can[i].first;
-        int feq=can[i].second;
-        for(int k=0;k<feq;k++){
-            set.push_back(x);
-            sum+=x;
-            fun(i+1,n,sum,t,can,ans,set);
-        }
-        for(int k=0;k<feq;k++){
-            set.pop_back();
-            // sum-=x;
-        }
-
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans(0,vector<int>(0));
-        vector<int> set(0);
-        int n=candidates.size();
-        unordered_map<int,int> um;
-        for(int i=0;i<n;i++){
-            um[candidates[i]]++;
-        }
-        vector<pair<int,int>> feq;
-        for (auto& [key, value] : um){
-            feq.push_back({key,value});
-        }
-        fun(0,feq.size(),0,target,feq,ans,set);
+        sort(candidates.begin(), candidates.end());               
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(0, candidates, target, curr, ans);
         return ans;
-
     }
 };
