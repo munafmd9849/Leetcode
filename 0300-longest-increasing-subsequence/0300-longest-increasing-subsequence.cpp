ac1a8099c073476234700dca57e6ed0c4633ad1e
Munaf <191462807+munafmd9849@users.noreply.github.com>
2026-07-04 01:22:40 +0530

    Time: 7 ms (74.02%), Space: 13.9 MB (94.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -17,23 +17,44 @@
 // };
 
 
+// class Solution {
+// public:
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         int ans=1;
+//         vector<int> dp(n+1,1);
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<=i;j++){
+//                 if(nums[j]<nums[i]){
+//                     dp[i]=max(dp[i],dp[j]+1);
+//                 }
+//             }
+//             ans=max(ans,dp[i]);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    
     int lengthOfLIS(vector<int>& nums) {
+        vector<int> tem;
+        tem.push_back(nums[0]);
         int n=nums.size();
-        int ans=1;
-        vector<int> dp(n+1,1);
         for(int i=1;i<n;i++){
-            for(int j=0;j<=i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i]=max(dp[i],dp[j]+1);
+            int j=0;
+            for(j;j<tem.size();j++){
+                if(nums[i]<=tem[j]){
+                    tem[j]=nums[i];
+                    break;
                 }
             }
-            ans=max(ans,dp[i]);
+            if(j==tem.size()){
+                tem.push_back(nums[i]);
+            }
         }
-        return ans;
+        return tem.size();
     }
 };
-
 
