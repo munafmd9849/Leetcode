a5fc3d389f6a12eedccd1774b4c75755f20485a5
Munaf <191462807+munafmd9849@users.noreply.github.com>
2026-08-01 23:33:44 +0530

    Time: 0 ms (100%), Space: 13.6 MB (78.7%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int searchInsert(vector<int>& arr, int k) {
-        int s=0,e=arr.size()-1;
-        int mid;
+        int s=0,e=arr.size()-1,ans=arr.size();
         while (s<=e){
-             mid=(e+s)/2;
+            int mid=(e+s)/2;
             if(arr[mid]==k){
                 return mid;
             }
-            else if(k<arr[mid]){
+            if(k<arr[mid]){
+                ans=mid;
                 e=mid-1;
             }
             else{
@@ -16,6 +16,6 @@ public:
             }
             
         }
-        return mid+1;
+        return ans;
     }
 };
